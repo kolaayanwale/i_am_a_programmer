116d7874f552f9c1305eabb296860e2b54aa6165
kolaayanwale <kmayanwale@yahoo.com>
2024-07-02 14:35:41 -0500

    update python file

--- python/coursera.py
+++ python/coursera.py
@@ -5,3 +5,250 @@ print(15+5+(3*2)/4**2+(3-7)*7)
 print(type(2)) #to tell you the data type
 average= 5
 print("The average size is:" + str(average)) #str converts data types to str
+def greeting(name, depertment):
+    print("welcome, "+ name)
+    print("You are part of "+depertment)
+
+greeting("Kola", "DevOps")
+
+def area(base,height):
+    return base*height/2 # to return values you want to use later. called function value is stored in a variable
+
+area_a= area(5,4)
+
+print(25 == 50 or 1 != 2) #equality operator (different from assign operator '=')
+print(not 1>2) #opposite answer, therefore will print true
+print("Wednesday" > "Friday") # compares unicode of letters. i.e sorts alphabetically
+
+def hint_username(username):
+    if len(username)<3:
+        print("Invalid. Username has to be at least 3 characters long")
+    elif len(username)>15: #elif is else if
+        print("Invalid. Username cannot be longer than 15 characters") 
+    else:
+        print("Valid username")
+
+hint_username("ka")
+print(7//2) # Use floor division - the integer
+print(7%2) # Use modulo division - the remainder
+
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempt " + str(x))
+        x += 1 # x=x+1
+    print("Done")
+    
+attempts(5)
+
+for variable_depending_on_in_values in range(5):
+    print(variable_depending_on_in_values)
+
+teams = ['Manchester City', 'Manchester United', 'Arsenal', 'Newcastle United']
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team + " vs " + away_team)
+
+pets = 'cats & dogs'
+pets.index('d')
+'Dragons' in pets # in acts as a conditional
+
+x = ['Now', 'we', 'are', 'cooking!']
+x.append('yeah')
+x.insert(0, 'Okay')
+x[2] = 'ARE'
+x.remove('Now')
+x.pop(0)
+
+# tuples
+full_name = ('Kola', 'M', 'Ayanwale')
+
+#iterate over lists and tuples
+animals = ['lion', 'Zebra']
+
+# enumerate function. index and value
+def skip_elements(elements):
+	results = []
+	for index, value in enumerate(elements):
+		if index % 2 == 0:
+			results.append(value)
+	return results
+
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+# list comprehensions
+def odd_numbers(n):
+	return [x for x in range(1,n+1) if x % 2 != 0]
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
+
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  wordlist = []
+  for word in words:
+    # Create the pig latin word and add it to the list
+    newword = word.replace(word[0],'') + word[0] + 'ay'
+    wordlist = wordlist.append(newword)
+    # Turn the list back into a phrase
+    break
+  return wordlist
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+years = ["January 2023", "May 2025", "April 2023", "August 2024", "September 2025", "December 2023"]
+
+# The list comprehension below creates a new list "updated_years" to
+# hold the command to replace the "2023" substring of the "year"
+# element with the substring "2024". This action will be executed if
+# the last 4 indices of the "year" string is equal to the substring
+# "2023". If false (else), the "year" element will be included in the
+# new list "updated_years" unchanged.
+updated_years = [year.replace("2023","2024") if year[-4:] == "2023" else year for year in years]
+
+
+print(updated_years) 
+# Should print ["January 2024", "May 2025", "April 2024", "August 2024", "September 2025", "December 2024"]
+
+# This function splits a given string into a list of elements. Then, it
+# modifies each element by moving the first character to the end of the 
+# element and adds a dash between the element and the moved character. 
+# For example, the element "2two" will be changed to "two-2". Finally,
+# the function converts the list back to a string, and returns the
+# new string.
+def change_string(given_string):
+
+# Initialize "new_string" as a string data type by using empty quotes.
+    new_string = ""
+    # Split the "given_string" into a "new_list", with each "element"
+    # holding an individual word from the string.
+    new_list = given_string.split()
+
+    # The for loop iterates over each "element" in the "new_list".
+    for element in new_list:
+
+        # Convert the list into a "new_string" by using the assignment
+        # operator += to concatenate the following items: 
+        # + Each list "element" (starting at index position [1:]), 
+        # + a dash "-", 
+        # + append the first character of the "element" (using the index 
+        # [0]) to the end of the "element", and finally,
+        # + a space " " to separate each "element" in the "new_string".
+        new_string += element[1:] + "-"  + element[0] + " "
+
+    # Return the list that has been converted back into a string.
+    return new_string
+
+
+print(change_string("1one 2two 3three 4four 5five")) 
+# Should print "one-1 two-2 three-3 four-4 five-5"  
+
+# Dictionaries
+# Skill 1
+# This function returns the total time, with minutes represented as 
+# decimals (example: 1 hour 30 minutes = 1.5), for all end user time
+# spent accessing a server in a given day. 
+
+
+def sum_server_use_time(Server):
+
+    # Initialize the variable as a float data type, which will be used
+    # to hold the sum of the total hours and minutes of server usage by
+    # end users in a day.
+    total_use_time = 0.0
+
+    # Iterate through the "Server" dictionary’s key and value items 
+    # using a for loop.
+    for key,value in Server.items():
+
+        # For each end user key, add the associated time value to the
+        # total sum of all end user use time.
+        total_use_time += Server[key]
+        
+    # Round the return value and limit to 2 decimal places.
+    return round(total_use_time, 2)  
+
+FileServer = {"EndUser1": 2.25, "EndUser2": 4.5, "EndUser3": 1, "EndUser4": 3.75, "EndUser5": 0.6, "EndUser6": 8}
+
+print(sum_server_use_time(FileServer)) # Should print 20.1
+
+# Skill 2
+# This function receives a dictionary, which contains common employee 
+# last names as keys, and a list of employee first names as values. 
+# The function generates a new list that contains each employees’ full
+# name (First_name Last_Name). For example, the key "Garcia" with the 
+# values ["Maria", "Hugo", "Lucia"] should be converted to a list 
+# that contains ["Maria Garcia", "Hugo Garcia", "Lucia Garcia"].
+
+
+def list_full_names(employee_dictionary):
+    # Initialize the "full_names" variable as a list data type using
+    # empty [] square brackets.  
+    full_names = []
+
+    # The outer for loop iterates through each "last_name" key and 
+    # associated "first_name" values, in the "employee_dictionary" items.
+    for last_name, first_names in employee_dictionary.items():
+
+        # The inner for loop iterates over each "first_name" value in 
+        # the list of "first_names" for one "last_name" key at a time.
+        for first_name in first_names:
+
+            # Append the new "full_names" list with the "first_name" value
+            # concatenated with a space " ", and the key "last_name". 
+            full_names.append(first_name+" "+last_name)
+            
+    # Return the new "full_names" list once the outer for loop has 
+    # completed all iterations. 
+    return(full_names)
+
+
+print(list_full_names({"Ali": ["Muhammad", "Amir", "Malik"], "Devi": ["Ram", "Amaira"], "Chen": ["Feng", "Li"]}))
+# Should print ['Muhammad Ali', 'Amir Ali', 'Malik Ali', 'Ram Devi', 'Amaira Devi', 'Feng Chen', 'Li Chen']
+
+# Skill 3
+# This function receives a dictionary, which contains resource 
+# categories (keys) with a list of available resources (values) for a 
+# company’s IT Department. The resources belong to multiple categories.
+# The function should reverse the keys and values to show which 
+# categories (values) each resource (key) belongs to. 
+
+
+def invert_resource_dict(resource_dictionary):
+  # Initialize a "new_dictionary" variable as a dict data type using
+  # empty {} curly brackets. 
+    new_dictionary = {}
+    # The outer for loop iterates through each "resource_group" and 
+    # associated "resources" in the "resource_dictionary" items.
+    for resource_group, resources in resource_dictionary.items():
+        # The inner for loop iterates over each "resource" value in 
+        # the list of "resources" for one "resource_group" key at a time.
+        for resource in resources:
+            # The if-statement checks if the current "resource" value has 
+            # been appended as a key to the "new_dictionary" yet.
+            if resource in new_dictionary:
+                # If True, then append the "resource_group" as a value to the
+                # "resource", which is now the key.
+                new_dictionary[resource].append(resource_group)
+            # If False (else), then add the "resource" as a new key with the 
+            # "resource_group" as a value for that key.
+            else:
+                new_dictionary[resource] = [resource_group]
+    # Return the new dictionary once the outer for loop has completed  
+    # all iterations.
+    return(new_dictionary)
+
+
+print(invert_resource_dict({"Hard Drives": ["IDE HDDs", "SCSI HDDs"],
+        "PC Parts":  ["IDE HDDs", "SCSI HDDs", "High-end video cards", "Basic video cards"], "Video Cards": ["High-end video cards", "Basic video cards"]}))
+# Should print {'IDE HDDs': ['Hard Drives', 'PC Parts'], 'SCSI HDDs': ['Hard Drives', 'PC Parts'], 'High-end video cards': ['PC Parts', 'Video Cards'], 'Basic video cards': ['PC Parts', 'Video Cards']}
+
